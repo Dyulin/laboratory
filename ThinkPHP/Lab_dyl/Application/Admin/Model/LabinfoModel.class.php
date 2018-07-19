8744fb979afe3c129eab18dfb77c3edcb551c658
Dyulin <40668726+Dyulin@users.noreply.github.com>
2018-07-19 21:11:45 +0800

    begin

---
+++ ThinkPHP/Lab_dyl/Application/Admin/Model/LabinfoModel.class.php
@@ -0,0 +1,30 @@
+<?php 
+namespace Admin\Model;
+use Think\Model;
+class LabinfoModel extends Model
+{
+	protected $tablename='user';
+	  public function log_in($work_id,$password)
+	    {
+	        /*$data = $this -> field('password')
+	      				->where('work_id=%d',$work_id)
+	      				->find();*/
+	      
+	      	$a = 'select work_id,password from user where work_id="'.$work_id.'"';
+	      	$data = $this -> query($a);	
+	   
+	      	
+	      	if($password===$data[0]['password'])
+	      	{	echo '登录成功';
+	      		return ture;
+			}else
+	      	{	echo '登录失败';
+	      		return false;
+	      	}
+	      	
+		} 
+
+	
+	 
+ }
+?>
